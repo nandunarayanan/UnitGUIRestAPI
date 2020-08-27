6dabfcfbb3d9c47af2837d07e513dfe10dd8550e
nandunarayanan <nanduremo@gmail.com>
2020-08-27 13:13:55 +0530

    Adding Readme.txt

---
+++ gitPull.sh
@@ -0,0 +1,8 @@
+echo "=================PWD=============="
+git config --global user.name "nandunarayanan"
+git config --global user.email "nanduremo@gmail.com"
+echo $USER
+echo "User Name"
+cd /home/bastin/UnitGUIRestAPI/
+echo "=================Pull Command=============="
+git pull orgin master
