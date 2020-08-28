cb92c1c9a3aa4eaa63732231345b9f46ed7b6f11
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-08-28 11:52:15 +0530

    Update ApiRun.sh

--- UnitGUIRestAPI/UnitTestFile/TestRunner/src/ApiRun.sh
+++ UnitGUIRestAPI/UnitTestFile/TestRunner/src/ApiRun.sh
@@ -1,1 +1,6 @@
+#! /bin/bash
+
+echo "======================Setting PWD========================"
+cd /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/UnitTestFile/TestRunner/src
+
 ./Gtest_Exec ApiTestCase.csv --gtest_output="xml:ReportFromLinuxGtestApi.xml"
