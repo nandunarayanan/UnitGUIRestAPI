7430b9160d125286f02459950c3fa1033576fc40
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-08-28 12:19:55 +0530

    Create ExecutionBot.sh

---
+++ UnitGUIRestAPI/ExecutionBot.sh
@@ -0,0 +1,4 @@
+echo "=================PWD=============="
+cd /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/testRobot
+echo "=================Transfering to Test Environment File=============="
+/usr/local/bin/robot ExecutionBot.robot
