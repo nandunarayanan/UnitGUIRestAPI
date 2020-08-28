74664d60b915537b808545ef1156c5e748c2b655
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-08-28 10:49:53 +0530

    Create ExecutionApi.sh

---
+++ UnitGUIRestAPI/testRobot/ExecutionApi.sh
@@ -0,0 +1,4 @@
+echo "=================PWD=============="
+cd /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/testRobot
+echo "=================Executing API_Exec from Target Environment=============="
+/usr/local/bin/robot ExecutionApi.robot
