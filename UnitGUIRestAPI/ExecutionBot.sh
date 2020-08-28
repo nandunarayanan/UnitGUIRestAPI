4703505de5cce78d6b949d200539927f91593562
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-08-28 12:20:33 +0530

    Delete ExecutionBot.sh

--- UnitGUIRestAPI/ExecutionBot.sh
+++
@@ -1,4 +0,0 @@
-echo "=================PWD=============="
-cd /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/testRobot
-echo "=================Transfering to Test Environment File=============="
-/usr/local/bin/robot ExecutionBot.robot
