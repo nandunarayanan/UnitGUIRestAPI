0acdd3f7b735b8faadc102bc514032ac81ffc649
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-08-31 15:20:59 +0530

    Update ApiRun.sh

--- UnitGUIRestAPI/UnitTestFile/TestRunner/src/ApiRun.sh
+++ UnitGUIRestAPI/UnitTestFile/TestRunner/src/ApiRun.sh
@@ -4,3 +4,4 @@ echo "======================Setting PWD========================"
 cd /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/UnitTestFile/TestRunner/src
 
 ./Gtest_Exec ApiTestCase.csv --gtest_output="xml:ReportFromLinuxGtestApi.xml"
+echo jenkins | sudo -S cp ReportFromLinuxGtestApi.xml ReportFromLinuxGtestBot.xml /var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/
