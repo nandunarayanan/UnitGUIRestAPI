a3a07fa3d41a0388c95441a5fa54dd00531e8e0b
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-09-01 20:26:24 +0530

    Update SquishWebRun.sh

--- UnitGUIRestAPI/GUITesting/SquishWebRun.sh
+++ UnitGUIRestAPI/GUITesting/SquishWebRun.sh
@@ -4,4 +4,4 @@ echo "======================Setting PWD========================"
 cd /home/bastin/squish-for-web-6.6.1/bin
 export SQUISH_LICENSEKEY_DIR="/home/bastin"
 echo "==============Running WEB API Application================"
-./squishrunner --host 192.168.43.13 --port 4322 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/GUITesting/suite_gwt1 --reportgen xml,/home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/GUITesting/Report/results_WebAPI.xml
+./squishrunner --host proxy61.rt3.io --port 36793 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/GUITesting/suite_gwt1
