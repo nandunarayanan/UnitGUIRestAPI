78fe6e92ae9445a0014dc2d1543bfba03038a944
nandunarayanan <nanduremo@gmail.com>
2020-09-07 13:31:25 +0530

    Updated for copying the xml to workspace location

--- UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
+++ UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
@@ -4,4 +4,7 @@ echo "======================Setting PWD========================"
 cd /home/bastin/squish-for-web-6.6.1/bin
 export SQUISH_LICENSEKEY_DIR="/home/bastin"
 echo "==============Running REST API Application================"
-./squishrunner --host proxy61.rt3.io --port 38986 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/DesktopGUITesting/suite_js --reportgen xmljunit,/var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/results_Desktop.xml
+./squishrunner --host localhost --port 4322 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/DesktopGUITesting/suite_js --reportgen xmljunit,results_Desktop.xml
+
+echo "=================Copying the generated file to the workspace=============="
+echo jenkins | sudo -S cp results_Desktop.xml /var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/
