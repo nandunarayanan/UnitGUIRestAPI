73a0a89a1894d198cbeab57833e86e2a671c0cad
nandunarayanan <nanduremo@gmail.com>
2020-09-07 12:37:35 +0530

    Updated the squish *.sh in all the squish test suites

--- UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
+++ UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
@@ -4,4 +4,4 @@ echo "======================Setting PWD========================"
 cd /home/bastin/squish-for-web-6.6.1/bin
 export SQUISH_LICENSEKEY_DIR="/home/bastin"
 echo "==============Running REST API Application================"
-./squishrunner --host proxy60.rt3.io --port 37412 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/RestfulTesting/TestSuite/suite_Ex_bdd_suite2 --reportgen xmljunit,/var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/results_Desktop.xml
+./squishrunner --host proxy61.rt3.io --port 38986 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/RestfulTesting/TestSuite/suite_Ex_bdd_suite2 --reportgen xmljunit,/var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/results_Desktop.xml
