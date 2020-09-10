b7545be466b3ca5c2c3fe88e1fe3d9caf769943d
nandunarayanan <nanduremo@gmail.com>
2020-09-10 16:52:40 +0530

    Updated local host into ip add

--- UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
+++ UnitGUIRestAPI/DesktopGUITesting/squishDesktopRun.sh
@@ -5,7 +5,7 @@ echo jenkins | sudo -S ls
 cd /home/bastin/squish-for-web-6.6.1/bin
 export SQUISH_LICENSEKEY_DIR="/home/bastin"
 echo "==============Running Desktop API Application================"
-./squishrunner --host localhost --port 4322 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/DesktopGUITesting/suite_js --reportgen xmljunit,results_Desktop.xml
+./squishrunner --host 192.168.43.71 --port 4322 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/DesktopGUITesting/suite_js --reportgen xmljunit,results_Desktop.xml
 
 echo "=================Copying the generated file to the workspace=============="
 echo jenkins | sudo -S cp results_Desktop.xml /var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/
