e89ecc1874c4ed3971d049687fc3ec0469f7ddca
nandunarayanan <nanduremo@gmail.com>
2020-09-10 11:57:16 +0530

    Updated proxy details for the squish server time

--- UnitGUIRestAPI/RestfulTesting/SquishRun.sh
+++ UnitGUIRestAPI/RestfulTesting/SquishRun.sh
@@ -4,4 +4,4 @@ echo "======================Setting PWD========================"
 cd /home/bastin/squish-for-web-6.6.1/bin
 export SQUISH_LICENSEKEY_DIR="/home/bastin"
 echo "==============Running REST API Application================"
-./squishrunner --host proxy60.rt3.io --port 38637 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/RestfulTesting/TestSuite/suite_Ex_bdd_suite2 --reportgen xmljunit,/var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/results_RestAPI.xml
+./squishrunner --host proxy61.rt3.io --port 30312 --testsuite /home/bastin/UnitGUIRestAPI/UnitGUIRestAPI/RestfulTesting/TestSuite/suite_Ex_bdd_suite2 --reportgen xmljunit,/var/lib/jenkins/workspace/UnitGUIRestfulAPI_Test/results_RestAPI.xml
