a33cafb8036cb76bb111adb25fb08a213398aba6
nandunarayanan <nanduremo@gmail.com>
2020-09-11 16:54:55 +0530

    Update the test.js to get fail when error occurs in jenkins with help of exceptioncondition fails in the CLI command

--- UnitGUIRestAPI/DesktopGUITesting/suite_js/tst_adding/test.js
+++ UnitGUIRestAPI/DesktopGUITesting/suite_js/tst_adding/test.js
@@ -31,7 +31,7 @@ function main()
     startApplication("addressbook");
     invokeMenuItem("File", "New");
     var table = waitForObject({"type": "QTableWidget"});
-    var returnVal = !(test.verify(table.rowCount == 0));
+    test.verify(table.rowCount == 0);
     var data = new Array(
         new Array("Andy", "Beach", "andy.beach@nowhere.com", "555 123 6786"),
         new Array("Candy", "Deane", "candy.deane@nowhere.com", "555 234 8765"),
@@ -39,11 +39,8 @@ function main()
     for (var row = 0; row < data.length; ++row)
         addNameAndAddress(data[row]);
 
-    //returnVal = (returnVal && !(test.compare(table.rowCount, data.length, "table contains as many rows as added data")));
-    var test_ret = test.compare(table.rowCount, 7, "table contains as many rows as added data")
-    returnVal = (returnVal || !(test_ret));
-    test.log("Retun Value: "+returnVal);
+    //test.compare(table.rowCount, data.length, "table contains as many rows as added data");
+    test.compare(table.rowCount, 7, "table contains as many rows as added data");
     closeWithoutSaving();
-    return returnVal;
 }
 
