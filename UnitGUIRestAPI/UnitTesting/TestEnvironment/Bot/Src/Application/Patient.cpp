cc000f860aa1b51dd718a790a88677df92948905
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-09-15 15:18:54 +0530

    Update Patient.cpp

--- UnitGUIRestAPI/UnitTesting/TestEnvironment/Bot/Src/Application/Patient.cpp
+++ UnitGUIRestAPI/UnitTesting/TestEnvironment/Bot/Src/Application/Patient.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <stdlib.h>
 
+/*!
+ * @brief default constructor
+ */
 Patient::Patient():
 	m_patientName(""),
 	m_age(0),
@@ -12,45 +15,79 @@ Patient::Patient():
 {
 }
 
+/*!
+ * @brief default destructor
+ */
 Patient::~Patient()
 {
 }
 
+/*!
+ * @brief This function returns the patient name
+ * @return the patient name
+ */
 std::string Patient::getPatientName()
 {
 	return m_patientName;
 }
 
+/*!
+ * @brief This function sets the patient name
+ * @param name patient name
+ */
 void Patient::setPatientName(std::string name)
 {
 	m_patientName = name;
 }
 
+/*!
+ * @brief This function sets the age
+ * @return patient age
+ */
 long Patient::getAge()
 {
 	return m_age;
 }
 
+/*!
+ * @brief This function returns the age of patient
+ * @param age is the patient age
+ */
 void Patient::setAge(long age)
 {
 	m_age = age;
 }
 
+/*!
+ * @brief This function returns the gender of patient
+ * @return the patient gender
+ */
 char Patient::getGender()
 {
 	return m_gender;
 }
 
+/*!
+ * @brief This function returns the gender of patient
+ * @param gender is the patient gender
+ */
 void Patient::setGender(char gender)
 {
 	m_gender = gender;
 }
 
+/*!
+ * @brief This function returns the patient ID
+ * @param ID is the patient
+ */
 std::string Patient::getPatientID()
 {
 	return m_patientID;
 }
 
+/*!
+ * @brief This function generates a random patientID
+ */
 void Patient::generatePatientID()
 {
     srand(time(0));
