2c6c85889612a7abd6c77d03177f0cea9aa6da6c
nandunarayanan <nanduremo@gmail.com>
2020-09-15 15:26:25 +0530

    Updated Application files in AUTRunner

--- UnitGUIRestAPI/UnitTesting/TestEnvironment/AUTRunner/Application/Battery.cpp
+++ UnitGUIRestAPI/UnitTesting/TestEnvironment/AUTRunner/Application/Battery.cpp
@@ -1,30 +1,48 @@
 #include "Battery.h"
 
+/*!
+ * @brief default constructor
+ */
 Battery::Battery()
 {
 
 }
-
+/*!
+ * @brief default destructor
+ */
 Battery::~Battery()
 {
 
 }
-
+/*!
+ * @brief This function returns the current battery percentage
+ * @return the current battery percentage
+ */
 double Battery::getBatteryPercentage()
 {
 	return m_batteryPercentage;
 }
-
+/*!
+ * @brief This function sets the current battery percentage
+ * @param current battery percentage
+ */
 void Battery::setBatteryPercentage(double percentage)
 {
 	m_batteryPercentage = percentage;
 }
-
+/*!
+ * @brief This function returns the current battery status
+ * @return current battery status
+ */
 BATT Battery::getBatteryStatus()
 {
 	return m_batteryStatus;
 }
 
+/*!
+ * @brief This function sets the current battery status
+ * @param current battery status
+ */
 void Battery::setBatteryStatus(BATT status)
 {
 	m_batteryStatus = status;
--- UnitGUIRestAPI/UnitTesting/TestEnvironment/AUTRunner/Application/Patient.cpp
+++ UnitGUIRestAPI/UnitTesting/TestEnvironment/AUTRunner/Application/Patient.cpp
@@ -3,7 +3,9 @@
 #include <time.h>
 #include <iostream>
 #include <stdlib.h>
-
+/*!
+ * @brief default constructor
+ */
 Patient::Patient():
 	m_patientName(""),
 	m_age(0),
@@ -11,46 +13,75 @@ Patient::Patient():
 	m_patientID("INVALID")
 {
 }
-
+/*!
+ * @brief default destructor
+ */
 Patient::~Patient()
 {
 }
-
+/*!
+ * @brief This function returns the patient name
+ * @return the patient name
+ */
+ 
 std::string Patient::getPatientName()
 {
 	return m_patientName;
 }
 
+/*!
+ * @brief This function sets the patient name
+ * @param name patient name
+ */
 void Patient::setPatientName(std::string name)
 {
 	m_patientName = name;
 }
-
+/*!
+ * @brief This function returns the age of patient
+ * @param age is the patient age
+ */
 long Patient::getAge()
 {
 	return m_age;
 }
-
+/*!
+ * @brief This function sets the age
+ * @return patient age
+ */
 void Patient::setAge(long age)
 {
 	m_age = age;
 }
 
+/*!
+ * @brief This function returns the gender of patient
+ * @return the patient gender
+ */
 char Patient::getGender()
 {
 	return m_gender;
 }
-
+/*!
+ * @brief This function returns the gender of patient
+ * @param gender is the patient gender
+ */
 void Patient::setGender(char gender)
 {
 	m_gender = gender;
 }
-
+/*!
+ * @brief This function returns the patient ID
+ * @param ID is the patient
+ */
 std::string Patient::getPatientID()
 {
 	return m_patientID;
 }
 
+/*!
+ * @brief This function generates a random patientID
+ */
 void Patient::generatePatientID()
 {
     srand(time(0));
