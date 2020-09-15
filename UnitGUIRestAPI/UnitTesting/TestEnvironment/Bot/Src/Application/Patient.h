66b0f6c303bd5ed04f7f107cc4904455a4dcd9bd
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-09-15 15:13:43 +0530

    Update Patient.h

--- UnitGUIRestAPI/UnitTesting/TestEnvironment/Bot/Src/Application/Patient.h
+++ UnitGUIRestAPI/UnitTesting/TestEnvironment/Bot/Src/Application/Patient.h
@@ -4,21 +4,69 @@
 class Patient
 {
 private:
-	std::string m_patientName;
-	long m_age;
-	char m_gender;
-	std::string m_patientID;
+    /*!
+     * @brief This enum is used to represent the current status of Battery
+     */
+    std::string m_patientName;
+    long m_age;
+    char m_gender;
+    std::string m_patientID;
 
-public:
-	Patient();
-	~Patient();
-	std::string getPatientName();
-	void setPatientName(std::string name);
-	long getAge();
-	void setAge(long age);
-	char getGender();
-	void setGender(char gender);
-	std::string getPatientID();
-	void generatePatientID();
+public:    
+    /*!
+     * @brief default constructor
+     */
+    Patient();
+    /*!
+     * @brief default destructor
+     */
+    ~Patient();
+    
+    /*!
+     * @brief This function returns the patient name
+     * @return the patient name
+     */
+    std::string getPatientName();
+    
+    /*!
+     * @brief This function sets the patient name
+     * @param name patient name
+     */
+    void setPatientName(std::string name);
+    
+    /*!
+     * @brief This function sets the age
+     * @return patient age
+     */
+    long getAge();
+    
+    /*!
+     * @brief This function returns the age of patient
+     * @param age is the patient age
+     */
+    void setAge(long age);
+    
+    /*!
+     * @brief This function returns the gender of patient
+     * @return the patient gender
+     */
+    char getGender();
+    
+    /*!
+     * @brief This function returns the gender of patient
+     * @param gender is the patient gender
+     */
+    void setGender(char gender);
+    
+    /*!
+     * @brief This function returns the patient ID
+     * @param ID is the patient
+     */
+    std::string getPatientID();
+    
+    /*!
+     * @brief This function generates a random patientID
+     */
+    void generatePatientID();
 };
 
